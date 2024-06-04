a70b5deed57e8c25a9d2c9cbe9f7c86101559f64
Manavarrp <manavarrp@gmail.com>
2024-06-04 14:40:44 -0500

    Connection DB, Entity, Repository and Controller Discount.

---
+++ src/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,102 @@
+﻿using CoreApiResponse;
+using Discount.API.Models;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DiscountController : BaseController
+    {
+        private readonly ICouponRepository _couponRepository;
+
+        public DiscountController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 30)]
+        public async Task<IActionResult> GetDiscount(string productId)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetDiscount(productId);
+                return CustomResult("Consulta exitosa", coupon);
+
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.Created)]
+        public async Task<IActionResult> CreateDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var isSaved = await _couponRepository.CreateDiscount(coupon);
+                if (isSaved)
+                {
+                    return CustomResult("Cupón creado",coupon);
+                }
+                return CustomResult("Hubo un error al crear el cupón", coupon);
+
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var isSaved = await _couponRepository.UpdateDiscount(coupon);
+                if (isSaved)
+                {
+                    return CustomResult("Cupón actualizado", coupon);
+                }
+                return CustomResult("Hubo un error al actualizar el cupón", coupon);
+
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteDiscount(string productId)
+        {
+            try
+            {
+                var isSaved = await _couponRepository.DeleteDiscount(productId);
+                if (isSaved)
+                {
+                    return CustomResult("Cupón eliminado");
+                }
+                return CustomResult("Hubo un error al eliminar el cupón");
+
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+
+            }
+        }
+    }
+}
---
+++ src/Discount.API/Repositories/ICouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.API.Models;
+
+namespace Discount.API.Repositories
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(string productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productId);
+    }
+}
