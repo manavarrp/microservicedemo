cca3870720af481719b6868eba3e923114861681
Manavarrp <manavarrp@gmail.com>
2024-06-03 21:47:53 -0500

    Basket Api Created

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -1,7 +1,3 @@
-using Catalog.Api.HostedService;
-using Catalog.Api.Interfaces.Manager;
-using Catalog.Api.Manager;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,20 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHostedService<AppHostedService>();
-builder.Services.AddScoped<IProductManager, ProductManager>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-   // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseAuthorization();
 
