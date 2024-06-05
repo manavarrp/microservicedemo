a50121e806b0f2f79af19826e63457ec79d8d200
Manavarrp <manavarrp@gmail.com>
2024-06-04 21:15:56 -0500

    Entity Order Creation

---
+++ src/Ordering/Ordering.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdateDate { get; set; }
+    }
+}
