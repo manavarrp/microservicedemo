f9a9bed4314c0a9b912cd8aaf91741cb968e3afb
Manavarrp <manavarrp@gmail.com>
2024-06-06 19:56:12 -0500

    Creation EventBusMessage project and add ref with basket

---
+++ src/BuildingBlocks/EventBus.Message/Event/BaseEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace EventBus.Message.Event
+{
+    public class BaseEvent
+    {
+        public BaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+        public Guid Id { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+    }
+}
